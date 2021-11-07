36e2ecad09b9564b78c12ffa04ce6f6ecef8301f
Buket <82089857+buketozceylan@users.noreply.github.com>
2021-11-07 13:48:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,22 @@ Guess = int(input("Guess: "))
 while n < 11:
     if Guess > n:
         print("Lower!")
-        score = score - 10
+        score -= 30
+        print("Score: " + str(score))
         Guess = int(input("Guess: "))
 
     elif Guess < n:
         print("Higher!")
-        score = score - 10
+        score -= 30
+        print("Score: " + str(score))
         Guess = int(input("Guess: "))
+    elif score == 0:
+        print("Your score is 0")
 
     elif Guess == n:
         print("True. Number: " + str(n))
+        print("Score: " + str(score))
         break
+    elif score == 0:
+        print("Your score is 0")
+        
